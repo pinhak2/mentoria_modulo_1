0cc71de897a0c6cde2fdf80bc0673cc88782795b
William Schneider <william.schneider@edu.pucrs.br>
2022-09-13 20:57:05 -0300

    insert data in html

--- modulo_1/scripts/main.js
+++ modulo_1/scripts/main.js
@@ -1,4 +1,5 @@
 let data;
+window.onload = checkUser();
 function goToHome() {
   location.href = "../Home/index.html";
 }
@@ -8,14 +9,60 @@ function goToUser() {
 }
 
 async function search() {
-  let inputElement = document.querySelector(`input`);
-
-  await fetch("https://api.github.com/users/" + inputElement.value)
+  user = localStorage.getItem("user");
+  let array = [];
+  const response = await fetch("https://api.github.com/users/" + user)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.login);
+      for (i in data) {
+        array[i] = data[i];
+      }
+    })
+    .finally(() => {
+      addDataToHTML(array);
     })
     .catch((error) => console.error(error));
-
   console.log("fechou");
+}
+
+function saveUser() {
+  let user = document.getElementById("user_search").value;
+  localStorage.setItem("user", user);
+}
+
+function checkUser() {
+  if (window.location.href.indexOf("User") > -1) return search();
+  return;
+}
+
+function addRow(div_id, data_value) {
+  const div = document.createElement("div");
+  div.innerHTML =
+    `
+    <p id=` +
+    div_id +
+    `> ` +
+    data_value +
+    `</p>
+  `;
+  document.getElementById(div_id).appendChild(div);
+}
+function addImage(data_value) {
+  const div = document.createElement("div");
+  div.innerHTML =
+    `
+    <img id="profilePicture" src=` +
+    data_value +
+    ` alt="user-image"/>
+  `;
+  document.getElementById("picture-box").appendChild(div);
+}
+
+function addDataToHTML(data) {
+  for (var i in data) {
+    if (document.getElementById(i)) {
+      if (data[i] !== null) addRow(i, data[i]);
+    }
+  }
+  addImage(data.avatar_url);
 }
